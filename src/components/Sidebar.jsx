098cf588d20dc448be9e8ee1163c0ce3649e4dfd
RandilT <randiltharusha72@gmail.com>
2024-02-08 13:58:58 +0530

    Create the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,85 @@
-import { Box } from "@mui/material";
+import {
+  AccountBox,
+  Drafts,
+  Group,
+  Home,
+  Inbox,
+  Person,
+  Report,
+  Settings,
+  Store,
+  Storefront,
+} from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 export const Sidebar = () => {
   return (
-    <Box
-      bgcolor="skyblue"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#groups">
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#store">
+          <ListItemIcon>
+            <Storefront />
+          </ListItemIcon>
+          <ListItemText primary="Marketplace" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#friends">
+          <ListItemIcon>
+            <Person />
+          </ListItemIcon>
+          <ListItemText primary="Friends" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#settings">
+          <ListItemIcon>
+            <Settings />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#profile">
+          <ListItemIcon>
+            <AccountBox />
+          </ListItemIcon>
+          <ListItemText primary="My Profile" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton component="a" href="#bugs">
+          <ListItemIcon>
+            <Report />
+          </ListItemIcon>
+          <ListItemText primary="Report" />
+        </ListItemButton>
+      </ListItem>
     </Box>
   );
 };
